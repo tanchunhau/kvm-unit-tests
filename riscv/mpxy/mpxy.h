eff47addb2ae0c8abaa2cf46c7bf12106d53b67e
ChunHau Tan <chunhau.tan@starfivetech.com>
2025-02-17 16:14:02 +0800

    t2

--- riscv/mpxy/mpxy.h
+++ riscv/mpxy/mpxy.h
@@ -32,7 +32,7 @@ int check_mpxy_clock(void);
 // clock service group
 int clock_srvgrp_probe(void);
 
-/**
+/*
 int clock_query_quantity(void);
 int clock_query_attrs(u32 clkid, struct sbi_rpxy_clk *rpxy_clk);
 int clock_query_supported_rates(u32 clkid, struct sbi_rpxy_clk *rpxy_clk);
@@ -51,4 +51,4 @@ int clock_negative_get_config_with_wrong_id(void);
 int clock_negative_set_rate_with_wrong_id(void);
 int clock_negative_set_rate_with_wrong_rate(void);
 int clock_negative_get_rate_with_wrong_id(void);
-/**/
+*/
