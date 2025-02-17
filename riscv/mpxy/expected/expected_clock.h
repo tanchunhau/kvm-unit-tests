f48f5eb73982aba5099d1db1c7b25e7cf1f6e122
ChunHau Tan <chunhau.tan@starfivetech.com>
2025-02-17 16:51:49 +0800

    a

--- riscv/mpxy/expected/expected_clock.h
+++ riscv/mpxy/expected/expected_clock.h
@@ -1,7 +1,7 @@
 #ifndef __MPXY_CLOCK_EXPECTED_H__
 #define __MPXY_CLOCK_EXPECTED_H__
 
-static const bool has_clock_srvgrp = false;
+static const bool has_clock_srvgrp = true;
 static const int expected_clock_tpid = 0x0b;
 
 #endif /* __MPXY_CLOCK_EXPECTED_H__ */
