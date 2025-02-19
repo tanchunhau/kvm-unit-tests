4f1feed215f550263ef6d4243571a7aa32aa5d41
ChunHau Tan <chunhau.tan@starfivetech.com>
2025-02-19 17:29:53 +0800

    a

--- riscv/sbi.c
+++ riscv/sbi.c
@@ -1442,9 +1442,9 @@ static void check_susp(void)
 static void check_mpxy(void)
 {
 	struct sbiret ret;
+	struct sbi_mpxy mpxy;
 	u32 *channel_ids = NULL;
 	long expected;
-	struct sbi_mpxy mpxy;
 
 	report_prefix_push("mpxy");
 
