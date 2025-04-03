cc583fb08a04705e41af38e7290fabb4cc85bee8
ChunHau Tan <chunhau.tan@starfivetech.com>
2025-04-03 13:19:33 +0800

    a

--- riscv/sbi.c
+++ riscv/sbi.c
@@ -1610,14 +1610,17 @@ static void check_mpxy(void)
 		memcpy(&num_domain_rx, mpxy.shmem, rx_bytes);
 	}
 	printf("status = %d\n", num_domain_rx.status);
-	printf("num_domains = %u\n", num_domain_rx.num_domains);
+	printf("num_domains = %u\n\n", num_domain_rx.num_domains);
 
 	struct rpmi_pm_get_domain_attrs_rx pm_get_domain_attrs_rx;
+	struct rpmi_pm_get_power_state_tx pm_get_power_state_tx;
+	struct rpmi_pm_get_power_state_rx pm_get_power_state_rx;
+
 
 	for (u32 i = 0; i < num_domain_rx.num_domains; i++) {
-		pm_get_domain_attrs_tx.domain_id = i;
 
-		/* Message protocols allowed to have no data in messages */
+		// get attribute
+		pm_get_domain_attrs_tx.domain_id = i;
 		if (sizeof(pm_get_domain_attrs_tx))
 			memcpy(mpxy.shmem, &pm_get_domain_attrs_tx, sizeof(pm_get_domain_attrs_tx));
 
@@ -1632,7 +1635,24 @@ static void check_mpxy(void)
 		printf("name[%u] = %s\n",i, pm_get_domain_attrs_rx.name);
 		printf("status = %d\n", pm_get_domain_attrs_rx.status);
 		printf("flags = %u\n", pm_get_domain_attrs_rx.flags);
-		printf("transition_latency = %u\n", pm_get_domain_attrs_rx.transition_latency);
+		printf("transition_latency = %u\n\n", pm_get_domain_attrs_rx.transition_latency);
+
+		// get state
+		pm_get_power_state_tx.domain_id = i;
+		if (sizeof(pm_get_power_state_tx))
+			memcpy(mpxy.shmem, &pm_get_power_state_tx, sizeof(pm_get_power_state_tx));
+
+		sret = sbi_ecall(SBI_EXT_MPXY, SBI_EXT_MPXY_SEND_MSG_WITH_RESP,
+			channel_ids[0], RPMI_DP_SRV_GET_STATE, sizeof(pm_get_power_state_tx), 0, 0, 0);
+
+		if (!sret.error) {
+			rx_bytes = sret.value;
+			rx_bytes = MIN(sizeof(pm_get_power_state_rx), rx_bytes);
+			memcpy(&pm_get_power_state_rx, mpxy.shmem, rx_bytes);
+		}
+		printf("status = %d\n", pm_get_power_state_rx.status);
+		printf("flags = %u\n", pm_get_power_state_rx.power_state);
+
 	}
 
 
