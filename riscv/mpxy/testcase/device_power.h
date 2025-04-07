f071edbedbdc08b58c75219560d96addd802a3f2
ChunHau Tan <chunhau.tan@starfivetech.com>
2025-04-07 15:20:04 +0800

    a

--- riscv/mpxy/testcase/device_power.h
+++ riscv/mpxy/testcase/device_power.h
@@ -1,8 +1,8 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
-void device_power_get_num_domains();
-void device_power_get_num_attributes();
-void device_power_get_state();
-void device_power_set_state();
+void device_power_get_num_domains(void);
+void device_power_get_num_attributes(void);
+void device_power_get_state(void);
+void device_power_set_state(void);
 
-void run_device_power_test();
+void run_device_power_test(void);
