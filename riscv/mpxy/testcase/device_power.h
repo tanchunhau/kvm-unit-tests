f5f632c0afc1836a569a61148a0ac4d9725d13cc
ChunHau Tan <chunhau.tan@starfivetech.com>
2025-04-08 10:38:26 +0800

    a

--- riscv/mpxy/testcase/device_power.h
+++ riscv/mpxy/testcase/device_power.h
@@ -5,5 +5,4 @@ void device_power_get_num_domains(struct sbi_mpxy* mpxy);
 void device_power_get_num_attributes(struct sbi_mpxy* mpxy);
 void device_power_get_state(struct sbi_mpxy* mpxy);
 void device_power_set_state(struct sbi_mpxy* mpxy);
-
 void run_device_power_test(struct sbi_mpxy* mpxy);
