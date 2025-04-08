2600e150b46e9675bf43bd05bd29eee5988f4fff
ChunHau Tan <chunhau.tan@starfivetech.com>
2025-04-08 15:12:40 +0800

    a

--- riscv/mpxy/testcase/device_power_expected.h
+++ riscv/mpxy/testcase/device_power_expected.h
@@ -5,17 +5,20 @@
 
 #include <libcflat.h>
 
+#define EXPECTED_NUMBER_OF_DOMAIN 6
+
+
 const bool has_device_power = true;
 const u32 channel_id = 4098;
-const u32 expected_number_of_domain = 6;
+const u32 expected_number_of_domain = EXPECTED_NUMBER_OF_DOMAIN;
 
-const char* expected_device_power_names[expected_number_of_domain] =
+const char* expected_device_power_names[EXPECTED_NUMBER_OF_DOMAIN] =
         {"dpwr0", "dpwr1", "dpwr2", "dpwr3", "dpwr4", "dpwr5"};
 
-const u32 expected_device_power_latencies[expected_number_of_domain] =
+const u32 expected_device_power_latencies[EXPECTED_NUMBER_OF_DOMAIN] =
         {100, 120, 90, 110, 130, 140};
 
-const u32 expected_device_power_flags[expected_number_of_domain] =
+const u32 expected_device_power_flags[EXPECTED_NUMBER_OF_DOMAIN] =
         {6, 6, 6, 6, 6, 6};
 
 #endif // _MPXY_DEVICE_POWER_EXPECTED_H
