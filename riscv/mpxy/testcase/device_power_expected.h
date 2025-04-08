6e54549116a2a6e95759682815417ef1c72b7046
ChunHau Tan <chunhau.tan@starfivetech.com>
2025-04-08 17:40:53 +0800

    here

--- riscv/mpxy/testcase/device_power_expected.h
+++ riscv/mpxy/testcase/device_power_expected.h
@@ -11,22 +11,22 @@ const u32 channel_id = 4098;
 #define EXPECTED_NUMBER_OF_DOMAIN 6
 
 const char* expected_device_power_names[EXPECTED_NUMBER_OF_DOMAIN] =
-        {"dpwr0", "dpwr1", "dpwr2", "dpwr3", "dpwr4", "dpwr5"};
+	{"dpwr0", "dpwr1", "dpwr2", "dpwr3", "dpwr4", "dpwr5"};
 
 const u32 expected_device_power_latencies[EXPECTED_NUMBER_OF_DOMAIN] =
-        {100, 120, 90, 110, 130, 140};
+	{100, 120, 90, 110, 130, 140};
 
 const u32 expected_device_power_flags[EXPECTED_NUMBER_OF_DOMAIN] =
-        {6, 6, 6, 6, 6, 6};
+	{6, 6, 6, 6, 6, 6};
 
 const u32 expected_get_device_power_states[EXPECTED_NUMBER_OF_DOMAIN] =
-        {RMPI_POWER_STATE_ON, RMPI_POWER_STATE_ON,
-         RMPI_POWER_STATE_ON, RMPI_POWER_STATE_ON,
-         RMPI_POWER_STATE_ON, RMPI_POWER_STATE_ON};
+	{RMPI_POWER_STATE_ON, RMPI_POWER_STATE_ON,
+	 RMPI_POWER_STATE_ON, RMPI_POWER_STATE_ON,
+	 RMPI_POWER_STATE_ON, RMPI_POWER_STATE_ON};
 
 const u32 expected_set_device_power_states[EXPECTED_NUMBER_OF_DOMAIN] =
-         {RMPI_POWER_STATE_OFF, RMPI_POWER_STATE_OFF,
-          RMPI_POWER_STATE_OFF, RMPI_POWER_STATE_OFF,
-          RMPI_POWER_STATE_OFF, RMPI_POWER_STATE_OFF};
+	{RMPI_POWER_STATE_OFF, RMPI_POWER_STATE_OFF,
+	 RMPI_POWER_STATE_OFF, RMPI_POWER_STATE_OFF,
+	 RMPI_POWER_STATE_OFF, RMPI_POWER_STATE_OFF};
 
 #endif // _MPXY_DEVICE_POWER_EXPECTED_H
