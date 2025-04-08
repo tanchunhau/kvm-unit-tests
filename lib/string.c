d8427412caeb2b78dc875c6b8da256259fa99458
ChunHau Tan <chunhau.tan@starfivetech.com>
2025-04-08 15:56:57 +0800

    a

--- lib/string.c
+++ lib/string.c
@@ -47,27 +47,9 @@ char *strcpy(char *dest, const char *src)
 
 int strncmp(const char *a, const char *b, size_t n)
 {
-	/*
 	for (; n--; ++a, ++b)
 		if (*a != *b || *a == '\0')
 			return *a - *b;
-
-	return 0;
-	*/
-
-	for (; n--; ++a, ++b) {
-		printf("n = %ld ", n);
-		if (*a != *b) {
-
-			printf("k1 %d\n", (*a - *b));
-			return *a - *b;
-		}
-
-		if (*a == '\0') {
-			printf("k2 %d\n", (*a - *b));
-			return *a - *b;
-		}
-	}
 
 	return 0;
 }
--- riscv/mpxy/testcase/device_power.c
+++ riscv/mpxy/testcase/device_power.c
@@ -62,13 +62,13 @@ int device_power_get_num_attributes(struct sbi_mpxy* mpxy)
 			memcpy(&pm_get_domain_attrs_rx, mpxy->shmem, rx_bytes);
 
 			if (pm_get_domain_attrs_rx.status != RPMI_SUCCESS)
-				{ret = MPXY_TEST_FAIL; printf("here 1\n");}
-			else if (strcmp(expected_device_power_names[i], pm_get_domain_attrs_rx.name) == 0)
-			{ret = MPXY_TEST_FAIL; printf("here 2\n");}
+				ret = MPXY_TEST_FAIL;
+			else if (strcmp(expected_device_power_names[i], pm_get_domain_attrs_rx.name) != 0)
+				ret = MPXY_TEST_FAIL;
 			else if (pm_get_domain_attrs_rx.transition_latency != expected_device_power_latencies[i])
-			{ret = MPXY_TEST_FAIL; printf("here 3\n");}
+				ret = MPXY_TEST_FAIL;
 			else if (pm_get_domain_attrs_rx.flags != expected_device_power_flags[i])
-			{ret = MPXY_TEST_FAIL; printf("here 4\n");}
+				ret = MPXY_TEST_FAIL;
 
 			printf("RPMI status[%u] = %s(%d)\n", i, getRPMIString(pm_get_domain_attrs_rx.status), pm_get_domain_attrs_rx.status);
 			printf("              name[%u] = %s, expected = %s\n",
